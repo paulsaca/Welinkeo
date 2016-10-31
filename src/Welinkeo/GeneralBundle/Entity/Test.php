dd09b1c9547d748c25a469d36c17fb218bcb6ed0
paulsaca <paulsacarabany@gmail.com>
2016-10-31 10:52:49 +0100

    finalisation configuration
    mise à jour à partir de composer et des fichiers de configuration

---
+++ src/Welinkeo/GeneralBundle/Entity/Test.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Welinkeo\GeneralBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Test
+ *
+ * @ORM\Table(name="test")
+ * @ORM\Entity(repositoryClass="Welinkeo\GeneralBundle\Repository\TestRepository")
+ */
+class Test
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
